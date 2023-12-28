b1ada3ea6845a37c421e91cdcaaf187bcc9cee24
imrkraghu <rohitgitpro@gmail.com>
2023-12-28 21:17:23 +0530

    added a function, removed print name line

--- hello.py
+++ hello.py
@@ -1,12 +1,13 @@
+# using a function to print the name and age
+def hello(to,much):
+    print(f"hello, Mr. {to} your age is {much}")
 # its my first python program
 name = input("what is your full name? ")
 #removing whitespace and capitalized name 
 name = name.strip().capitalize()
 # spliting the name into first and last name
 first, last =  name.split(" ")
-# we overrided separator here 
-print("hello, Mr. ",last,sep="")
 # taking age as input
 age = int(input("enter your age: "))
 # using f-string here
-print(f"hello, Mr. {last} your age is {age}")
+hello(last,age)
