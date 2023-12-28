c320a55cb7a8b49e555c1fea187c2bf21c8e5a32
imrkraghu <rohitgitpro@gmail.com>
2023-12-28 20:56:39 +0530

    sepparated name into first and last two variables

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 # its my first python program
-name = input("what is your name? ")
+name = input("what is your full name? ")
 #removing whitespace and capitalized name 
 name = name.strip().capitalize()
+# spliting the name into first and last name
+first, last =  name.split(" ")
 # we overrided separator here 
-print("hello, ",name,sep="")
+print("hello, Mr. ",last,sep="")
 # taking age as input
 age = int(input("enter your age: "))
 # using f-string here
-print(f"hello, {name} your age is {age}")
+print(f"hello, Mr. {last} your age is {age}")
