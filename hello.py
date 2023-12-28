14c7b590a8ae5e05ef43f6d943f031d4d715d589
imrkraghu <rohitgitpro@gmail.com>
2023-12-28 20:25:15 +0530

    added age and used f string

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 # its my first python program
 name = input("what is your name? ")
-print("hello, ",name)
+# we overrided separator here 
+print("hello, ",name,sep="")
+# taking age as input
+age = int(input("enter your age: "))
+# using f-string here
+print(f"hello, {name} your age is {age}")
