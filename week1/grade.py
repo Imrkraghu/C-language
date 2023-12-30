79ba6977c3e4b11ffe64f50a3ebc645d205c53d4
imrkraghu <rohitgitpro@gmail.com>
2023-12-30 12:41:36 +0530

    removed and and improved question statement

--- week1/grade.py
+++ week1/grade.py
@@ -1,12 +1,12 @@
 #this is second practice file
 score = int(input("Score: "))
-if score>=90 and score <=100:
+if score>=90<=100:
     print("Grade: A")
-elif score>=80 and score<90:
+elif score>=80<90:
     print("Grade: B")
-elif score>=70 and score<80:
+elif score>=70<80:
     print("Grade: C")
-elif score>=60 and score<70:
+elif score>=60<70:
     print("Grade: D")
 else:
     print("Grade: F") 
