38ba5fae659a5cc1d597e0dbe5c6d0c967b2a0fb
imrkraghu <rohitgitpro@gmail.com>
2023-12-30 12:30:08 +0530

    changed question to check only if x and y is equal

--- week1/practice.py
+++ week1/practice.py
@@ -3,11 +3,5 @@
 x = int(input("what is x? "))
 y = int(input("what is y? "))
 # this is only if condition being followed
-if x<y:
-    print("x is less than y")
-#using elif condition
-elif x>y:
-    print("x is greater than y")
-#using else statement
-else:
-    print("x is equal to y")
+if x<y or x>y:
+    print("x is not equal to y")
