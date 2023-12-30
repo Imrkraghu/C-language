3930efd67dcddab65ffb8b91c3a3b5c858322fe8
imrkraghu <rohitgitpro@gmail.com>
2023-12-30 12:20:52 +0530

    practice if condition

--- week1/practice.py
+++ week1/practice.py
@@ -1,2 +1,11 @@
 #this is used to revise whats in the video
 #today learning conditionals
+x = int(input("what is x? "))
+y = int(input("what is y? "))
+
+if x<y:
+    print("x is less than y")
+if x>y:
+    print("x is greater than y")
+if x==y:
+    print("x is equal to y")
