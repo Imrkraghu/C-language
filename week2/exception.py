900d833d18b3459d1a18227d890aec3e53c77750
Rohit Kumar <rohitgitpro@gmail.com>
2024-11-28 12:27:12 +0530

    added  while loop so that it asks for integer input continusly when value error

--- week2/exception.py
+++ week2/exception.py
@@ -1,7 +1,8 @@
 #this is cs50 exception handling program
-try:
-    x = int(input("what's x? "))
-except ValueError:
-    print("x is not an integer")
-else:
-    print(f"x is {x}")
+while True:
+    try:
+        x = int(input("what's x? "))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        print(f"x is {x}")
